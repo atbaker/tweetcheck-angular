84ef5b7d7a7e63ba8b1950bd8b5f70a513e17e81
Andrew T. Baker <andrew.tork.baker@gmail.com>
2014-12-02 18:23:05 -0500

    More work on activity log

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,3 +1,23 @@
 /* Filters */
 
-angular.module('tweetCheck.filters', []);
+angular.module('tweetCheck.filters', [])
+
+.filter('summarize', function () {
+    return function (value, wordwise, max) {
+        if (!value) return '';
+
+        max = (parseInt(max, 10) || 30);
+        if (!max) return value;
+        if (value.length <= max) return value;
+
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace != -1) {
+                value = value.substr(0, lastspace);
+            }
+        }
+
+        return value + (' …');
+    };
+});
